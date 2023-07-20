9488069d26eab88bc9cf246426d26cbfb732c1e7
Sye-5pace <owusilasyp00@gmail.com>
2023-07-20 05:33:40 +0100

    Font-switcher feature fixed ,defects resolved

---
+++ font-switcher.ts
@@ -0,0 +1,24 @@
+
+
+export const applyFont = (font) => {
+    const body = document.querySelector('body') as HTMLBodyElement;
+    const inputField = body.querySelector('input') as HTMLInputElement;
+    const word = document.querySelector('#word-phonetic > h1') as HTMLHeadingElement 
+    if (font === 'serif') {
+        body.style.fontFamily = 'Trirong, serif';
+        inputField.style.fontFamily = 'Trirong, serif';
+        word.style.fontFamily = 'Trirong, serif';
+    } else if (font === 'sans') {
+        body.style.fontFamily = 'Poppins, sans-serif';
+        inputField.style.fontFamily = 'Poppins, sans-serif';
+        word.style.fontFamily = 'Poppins, sans-serif';
+    } else if (font === 'mono') {
+        body.style.fontFamily = 'Azeret, monospace';
+        inputField.style.fontFamily = 'Azeret, monospace';
+        word.style.fontFamily = 'Azeret, monospace';
+    }
+}
+
+export const loadSelected = ()=>{
+    return localStorage.getItem('selectedFont');
+}
--- index.ts
+++ index.ts
@@ -1,30 +1,49 @@
+import { loadTheme, toggleTheme } from './theme-switcher'
+import { applyFont,loadSelected } from './font-switcher.ts'
 
 
-document.addEventListener('DOMContentLoaded',() => {
-        //importing interface for word type
-        //font-switcher
-        const fontGroup = document.getElementById('font-group') as HTMLSelectElement;
-        let selectedFont= localStorage.getItem('selectedFont');
-        
-        document.body.classList.add(`font-${selectedFont}`);
+// import { loadTheme, toggleTheme } from './theme-switcher'
 
-        fontGroup.addEventListener('change',()=> {
-                const newFont = fontGroup.value;
-                document.body.classList.remove(`font-${selectedFont}`);
-                document.body.classList.add(`font-${newFont}`);
+document.addEventListener('DOMContentLoaded', () => {
+    // let fontValue: string = document.querySelector('select').value
+    const fontGroup = document.querySelector('select')
 
-                selectedFont = newFont;
-                localStorage.setItem('selectedFont', selectedFont);
-        })
+    const savedFont = loadSelected();
+    if(savedFont){
+        fontGroup.value = savedFont;
+        applyFont(savedFont)
+    }
 
 
+    fontGroup.addEventListener('change',(e) => {
+        let newFont = e.currentTarget.value
+        console.log('Old font: ' + savedFont);
+        console.log('Selected font: ' + newFont);
+        applyFont(newFont);
 
-        const switchTheme = document.getElementById('switch');
-        const dark = document.getElementById('dark');
-//    const switchContainer = document.getElementById("switch-container");
+        localStorage.setItem('selectedFont', newFont)
+    })
+});
+  
+// const fontGroup = document.getElementById('font-group') as HTMLSelectElement;
+// const defaultFont = 'serif';
+// let selectedFont = localStorage.getItem('selectedFont') || defaultFont;
 
+// const applyFont = (font) => {
+// document.body.classList.remove(`font-${selectedFont}`);
+// document.body.classList.add(`font-${font}`);
+// document.getElementById('word-search')?.classList.remove(`font-${selectedFont}`);
+// document.getElementById('word-search')?.classList.add(`font-${font}`);
+// document.getElementById('word-phonetic')?.classList.remove(`font-${selectedFont}`);
+// document.getElementById('word-phonetic')?.classList.add(`font-${font}`);
+// selectedFont = font; // Update the selectedFont to the new value after applying the font.
+// };
 
-   dark.addEventListener("click",() => {
-        switchTheme.classList.add('switch-dark');
-   })
-})
+// fontGroup.value = selectedFont;
+// applyFont(selectedFont);
+
+// fontGroup.addEventListener('change', () => {
+//     const newFont = fontGroup.value;
+//     applyFont(newFont);
+//     localStorage.setItem('selectedFont', newFont); // Store the newFont value in localStorage.
+// });
